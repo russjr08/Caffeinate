5416bf75d7e4ac17a63dab71475491a924387e94
Russell Richardson <Russell@KronosAD.com>
2016-06-23 00:51:57 -0500

    Added sound profile tile

---
+++ app/src/main/java/xyz/omnicron/caffeinate/services/SoundProfileTileService.kt
@@ -0,0 +1,71 @@
+package xyz.omnicron.caffeinate.services
+
+import android.content.Context
+import android.graphics.drawable.Icon
+import android.media.AudioManager
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import xyz.omnicron.caffeinate.R
+
+/**
+* @author russjr08
+*/
+class SoundProfileTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+
+        val audio: AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        if(audio.ringerMode == AudioManager.RINGER_MODE_NORMAL) {
+            qsTile?.label = getString(R.string.soundprofile_tile_label_ringer)
+        } else {
+            qsTile?.label = getString(R.string.soundprofile_tile_label_vibrate)
+        }
+
+
+        qsTile?.state = Tile.STATE_ACTIVE
+        qsTile?.updateTile()
+    }
+
+    override fun onClick() {
+        super.onClick()
+
+        val audio: AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        // Tile should ALWAYS be active.
+        if(qsTile.state == Tile.STATE_INACTIVE) {
+            qsTile.state = Tile.STATE_ACTIVE
+        }
+
+        // Check if tile has been initialized, if not, initialize it.
+//        if(qsTile.label != getString(R.string.soundprofile_tile_label_vibrate)
+//                && qsTile.label != getString(R.string.soundprofile_tile_label_ringer)) {
+//
+//            if(audio.ringerMode == AudioManager.RINGER_MODE_NORMAL) {
+//                qsTile.label = getString(R.string.soundprofile_tile_label_ringer)
+//            } else {
+//                qsTile.label = getString(R.string.soundprofile_tile_label_vibrate)
+//            }
+//
+//            qsTile.updateTile()
+//
+//            return
+//        }
+
+        if(qsTile.label == getString(R.string.soundprofile_tile_label_ringer)) {
+            audio.ringerMode = AudioManager.RINGER_MODE_VIBRATE
+            qsTile.label = getString(R.string.soundprofile_tile_label_vibrate)
+            qsTile.icon = Icon.createWithResource(this, R.drawable.ic_vibration_white_24dp)
+        } else {
+            audio.ringerMode = AudioManager.RINGER_MODE_NORMAL
+            qsTile.label = getString(R.string.soundprofile_tile_label_ringer)
+            qsTile.icon = Icon.createWithResource(this, R.drawable.ic_notifications_active_white_24dp)
+
+        }
+
+        qsTile.updateTile()
+
+    }
+
+}
