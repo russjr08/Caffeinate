33547994d02d10924a1d5d70ff7982cc4c727e39
Russell <russjr08@gmail.com>
2017-11-01 23:15:36 -0400

    Sound Profile Tile enhancements
    Can now request permission to bypass DND mode, allowing users to change ringer mode even while phone is in DND.

--- app/src/main/java/xyz/omnicron/caffeinate/services/SoundProfileTileService.kt
+++ app/src/main/java/xyz/omnicron/caffeinate/services/SoundProfileTileService.kt
@@ -2,10 +2,13 @@ package xyz.omnicron.caffeinate.services
 
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.Icon
 import android.media.AudioManager
 import android.provider.Settings
 import android.service.quicksettings.TileService
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.MaterialDialog
 import xyz.omnicron.caffeinate.R
 
 /**
@@ -20,8 +23,10 @@ class SoundProfileTileService : TileService() {
 
         if(audio.ringerMode == AudioManager.RINGER_MODE_NORMAL) {
             qsTile?.label = getString(R.string.soundprofile_tile_label_ringer)
+            qsTile.icon = Icon.createWithResource(this, R.drawable.ic_notifications_active_white_24dp)
         } else {
             qsTile?.label = getString(R.string.soundprofile_tile_label_vibrate)
+            qsTile.icon = Icon.createWithResource(this, R.drawable.ic_vibration_white_24dp)
         }
 
         qsTile?.updateTile()
@@ -33,7 +38,7 @@ class SoundProfileTileService : TileService() {
         val audio: AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
         if(!isPhoneInDoNotDisturb()) {
-            if (qsTile.label == getString(R.string.soundprofile_tile_label_ringer)) {
+            if (audio.ringerMode == AudioManager.RINGER_MODE_NORMAL) {
                 audio.ringerMode = AudioManager.RINGER_MODE_VIBRATE
                 qsTile.label = getString(R.string.soundprofile_tile_label_vibrate)
                 qsTile.icon = Icon.createWithResource(this, R.drawable.ic_vibration_white_24dp)
@@ -43,6 +48,18 @@ class SoundProfileTileService : TileService() {
                 qsTile.icon = Icon.createWithResource(this, R.drawable.ic_notifications_active_white_24dp)
 
             }
+        } else {
+            val dialog = MaterialDialog.Builder(applicationContext)
+                    .title(R.string.sound_dialog_dnd_error_title)
+                    .content(R.string.sound_dialog_dnd_error_message)
+                    .positiveText("Grant Permission")
+                    .neutralText("Cancel")
+                    .onPositive(MaterialDialog.SingleButtonCallback { _: MaterialDialog, _: DialogAction ->
+                        startActivity(Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS))
+                    })
+                    .build()
+
+            showDialog(dialog)
         }
 
         qsTile.updateTile()
@@ -51,16 +68,12 @@ class SoundProfileTileService : TileService() {
 
     // https://stackoverflow.com/questions/31387137/android-detect-do-not-disturb-status
     fun isPhoneInDoNotDisturb() : Boolean {
-
         val notificationManager: NotificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if(!notificationManager.isNotificationPolicyAccessGranted) {
-            return true
-        }
-        try {
-            val mode = Settings.Global.getInt(contentResolver, "zen_mode")
-            return mode != 0
-        } catch (e: Settings.SettingNotFoundException) {
+
+        if (Settings.Global.getInt(contentResolver, "zen_mode") == 0) { // 0 is normal mode
             return false
+        } else {
+            return !notificationManager.isNotificationPolicyAccessGranted
         }
     }
 
